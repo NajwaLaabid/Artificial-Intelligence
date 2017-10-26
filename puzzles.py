02f2ed848df629eb8927e81c46db76a6f1597e88
Najwa Laabid <najwalaabid@gmail.com>
2017-10-26 01:18:10 +0100

    added dfs

--- puzzles.py
+++ puzzles.py
@@ -10,8 +10,8 @@ class Node:
 	depth = 0
 
 	def __init__(self, state, parent, operator, path_cost, depth):
-		self.state  = state
-		self.parent = parent
+		self.state = copy.deepcopy(state)
+		self.parent = copy.deepcopy(parent)
 		self.operator = operator
 		self.path_cost = path_cost
 		self.depth = depth
@@ -33,9 +33,17 @@ class Puzzle:
 		self.initState(size, self.moves_array[diff - 1])
 
 	def move(self, idx1, idx2):
-		temp = self.curstate[idx1['z']][idx1['x']][idx1['y']] 
-		self.curstate[idx1['z']][idx1['x']][idx1['y']] =  self.curstate[idx2['z']][idx2['x']][idx2['y']] 
-		self.curstate[idx2['z']][idx2['x']][idx2['y']] = temp
+		'''print("before move : ")
+		print(idx1)
+		print(self.curstate[self.size - idx1['z'] - 1][idx1['x']][idx1['y']])
+		print(idx2)
+		#print(idx2)'''
+		temp = copy.deepcopy(self.curstate[self.size - idx1['z'] - 1][idx1['x']][idx1['y']])
+		self.curstate[self.size - idx1['z'] - 1][idx1['x']][idx1['y']] =  copy.deepcopy(self.curstate[self.size - idx2['z'] - 1][idx2['x']][idx2['y']])
+		self.curstate[self.size - idx2['z'] - 1][idx2['x']][idx2['y']] = copy.deepcopy(temp)
+		#print('after move')
+		#print(self.curstate[self.size - idx1['z'] - 1][idx1['x']][idx1['y']])
+
 
 	def fillGrid(self):
 		i = 0
@@ -63,14 +71,15 @@ class Puzzle:
 		elif goal_choice == 3: #reverse order, bottom-right
 			self.numbers = list(reversed(self.numbers))
 			self.numbers.append(0)
-			self.goal = self.fillGrid()
+			self.goal = copy.deepcopy(self.fillGrid())
 
 	def findEmptyTile(self, state):
 		for z in range(self.size):
 			for x in range(self.size):
 				for y in range(self.size):
 					if state[z][x][y] == 0:
-						idx = {"x" : x, "y": y, "z" : z}
+						#idx = {"x" : self.size - x, "y": self.size - y, "z" : self.size - z}
+						idx = {"x" : x, "y": y, "z" : self.size - z - 1}
 						return idx
 
 	def initState(self, size, moves):
@@ -79,16 +88,17 @@ class Puzzle:
 		while(i < moves):
 			random.shuffle(self.directions[size - 2]) #first list in directions has moves for 2D
 			move = self.directions[size - 2][0]
+			#move = 'up'
 			idxTile = {}
 			idxTile = self.findEmptyTile(self.curstate)
 			if move == 'up':
 				idxTile2 = idxTile.copy()
-				if idxTile2['z'] > 0: idxTile2['z'] -= 1 #ignore state
+				if idxTile2['z'] < self.size - 1: idxTile2['z'] += 1 #ignore state
 				else: i -= 1
 				self.move(idxTile, idxTile2)
 			elif move == 'down':
 				idxTile2 = idxTile.copy()
-				if idxTile2['z'] < 2: idxTile2['z'] += 1
+				if idxTile2['z'] > 0: idxTile2['z'] -= 1
 				else: i -= 1
 				self.move(idxTile, idxTile2)
 			elif move == 'left':
@@ -98,7 +108,7 @@ class Puzzle:
 				self.move(idxTile, idxTile2)
 			elif move == 'right':
 				idxTile2 = idxTile.copy()
-				if idxTile2['x'] < 2: idxTile2['x'] += 1
+				if idxTile2['x'] < self.size - 1: idxTile2['x'] += 1
 				else: i -= 1
 				self.move(idxTile, idxTile2)
 			elif move == 'front':
@@ -108,7 +118,7 @@ class Puzzle:
 				self.move(idxTile, idxTile2)
 			elif move == 'back':
 				idxTile2 = idxTile.copy()
-				if idxTile2['y'] < 2: idxTile2['y'] += 1
+				if idxTile2['y'] < self.size - 1: idxTile2['y'] += 1
 				else: i -= 1
 				self.move(idxTile, idxTile2)
 			i += 1
@@ -126,13 +136,18 @@ class Puzzle:
 			return 0;
 
 	def frontierNodes(self, actions, parent, path_cost, depth):
+		print("in frontier path cost")
+		print(path_cost)
 		fn = []
 		for a in actions:
+			self.curstate = copy.deepcopy(parent)
+			print("before calling createNode")
 			fn.append(self.createNode(parent, a, path_cost, depth))
 
 		return fn
-
+ 
 	def DFS(self, parent, stack, closed_list, path_cost):
+
 		#general search algo 
 		#initial state => possible actions => nodes => building tree
 		#run DFS on tree => stack managing frontier => call testGoal on each node 
@@ -145,26 +160,40 @@ class Puzzle:
 
 		#questions:
 		#path cost same as depth in DFS
+		'''print("closed_list")
+		for c in closed_list:
+			self.PrintState(c)'''
 		closed_list.append(parent);
+		
 		path_cost += 1
+		print("cost")
+		print(path_cost)
 		flag = 0
 		#base case 
-		if self.testGoal(parent) == 1:
-			return parent;
+		#print(self.testGoal(parent))
+		if self.testGoal(parent.state):
+			return parent
 
 		#recursive algo
 		else:
-			actions = self.frontierActions(parent) #generate possible actions from parent
-			frontier = self.frontierNodes(actions, parent, path_cost, path_cost) #check parent to avoid loops
+			actions = self.frontierActions(parent.state) #generate possible actions from parent
+			frontier = self.frontierNodes(actions, parent.state, path_cost, path_cost) #check parent to avoid loops
+			print("parent: ")
+			self.PrintState(parent.state)
+			print("children: ")
 			for n in frontier: 
 				if n not in closed_list:
 					flag = 1
+					print(n.operator)
+					self.PrintState(n.state)
 					stack.append(n)
-			if flag:
-				path_cost = 1
+				else:
+					parent.path_cost += 1
 			#if parent already visited pop next
-			parent = stack.pop()
-			self.DFS(parent.state, stack, closed_list, path_cost)
+			if not stack:
+				return parent
+			parent = copy.deepcopy(stack.pop())
+			return self.DFS(parent, stack, closed_list, path_cost)
 
 
 	def frontierActions(self, state):
@@ -182,39 +211,57 @@ class Puzzle:
 		return possibleActions;
 
 	def createNode(self, parent, action, path_cost, depth):
+		print("in node")
+		print(path_cost)
 		idx1 = self.findEmptyTile(parent)
-		#generate state using move function
-		self.curstate = parent
+
 		if action == 'up':
-			idx2 = {'x' : idx1['x'], 'y': idx1['y'], 'z' : idx1['z'] + 1}
+			idx2 = idx1.copy()
+			idx2['z'] += 1
 			self.move(idx1, idx2)
 		elif action == 'down':
-			idx2 = {'x' : idx1['x'], 'y': idx1['y'], 'z' : idx1['z'] - 1}
+			idx2 = idx1.copy()
+			idx2['z'] -= 1
 			self.move(idx1, idx2)
 		elif action == 'left':
-			idx2 = {'x' : idx1['x'] - 1, 'y': idx1['y'], 'z' : idx1['z']}
+			idx2 = idx1.copy()
+			idx2['x'] -= 1
 			self.move(idx1, idx2)
 		elif action == 'right':
-			idx2 = {'x' : idx1['x'] + 1, 'y': idx1['y'], 'z' : idx1['z']}
+			idx2 = idx1.copy()
+			idx2['x'] += 1
 			self.move(idx1, idx2)
 		elif action == 'back':
-			idx2 = {'x' : idx1['x'], 'y': idx1['y'] + 1, 'z' : idx1['z']}
+			idx2 = idx1.copy()
+			idx2['y'] += 1
 			self.move(idx1, idx2)
 		elif action == 'front':
-			idx2 = {'x' : idx1['x'], 'y': idx1['y'] - 1, 'z' : idx1['z']}
+			idx2 = idx1.copy()
+			idx2['y'] -= 1
 			self.move(idx1, idx2)
 
-		#generate node and return it 
 		return Node(self.curstate, parent, action, path_cost, depth)
 
 
 if __name__ == '__main__':
-	puzzle = Puzzle(3, 2, 2);
+
+	puzzle = Puzzle(3, 2, 1);
 	print('initial state')
 	puzzle.PrintState(puzzle.initial)
-	print('possible actions')
+	#print("empty tile:")
+	#print(puzzle.findEmptyTile(puzzle.initial))
+	print('goal state')
+	puzzle.PrintState(puzzle.goal)
+	#print('curstate')
+	#puzzle.PrintState(puzzle.curstate)
+	#print('possible actions')
 	stack = []
 	closed_list = []
 	path_cost = 0
-	print(puzzle.DFS(puzzle.initial, stack, closed_list, path_cost))
+	node = Node(puzzle.initial, [], '', 0, 0)
+	goal = puzzle.DFS(node, stack, closed_list, path_cost)
+
+	print("goal path cost")
+	print(goal.path_cost)
+
 	
